4f0567538934fbe8859f079883ac58e02d6753ed
bensonZhang <bz7843@rit.edu>
2026-03-24 15:14:00 -0400

    Random items spawn
    Random items now spawn, though there is an index out of range - Ryan

--- Assets/Scripts/SpawnItems.cs
+++ Assets/Scripts/SpawnItems.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class SpawnItems : MonoBehaviour
 {
     public List<GameObject> spawnObjects;
-
+    public int numberOfItems;
     void Start()
     {
-        Instantiate(spawnObjects[0], transform.position, Quaternion.identity);
+        //3 of each item will spawn
+        numberOfItems = spawnObjects.Count * 3;
+        int currentItem = 0;
+        for (int i = 0; i < numberOfItems; i++) { 
+            if (numberOfItems % 3 == 0)
+            {
+                currentItem++;
+            }
+            Vector3 itemPosition = transform.position;
+            itemPosition.x += Random.Range(-10, 10);
+            itemPosition.y += Random.Range(-10, 10);
+            Instantiate(spawnObjects[currentItem], itemPosition, Quaternion.identity);
+        }
+        
     }
 
     // Update is called once per frame
